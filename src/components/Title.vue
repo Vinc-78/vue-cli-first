080fc49450c22a81c591e7bda7e584ed3707a402
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-11-10 23:37:54 +0100

    completato prima app

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <h1>{{ titolo }}</h1>
+
+</template>
+
+<script>
+export default {
+    name: 'Title',
+    props: {
+        titolo: String 
+    }
+}
+</script>
+
+<style>
+    
+</style>
